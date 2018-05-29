2b8337c3bf470549dfa11d54cd08bed130abc6dc
ryo.nakajima <ryo.nakajima@rakuten.com>
2018-05-29 06:09:53 +0000

    This commit aim to submit work. Add by Ryo Nakajima

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// add by Ryo Nakajima 2018/05/29
+Route::get('/', 'TaskController@index');
+Route::resource('tasks', 'TaskController');
